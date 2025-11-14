91a595691ca06750ffb5fbee163eee891105b9c9
scotty974 <fabienetheve@hotmail.com>
2025-11-14 12:34:12 +0100

    fix (bug) fix size retries

--- airflow/dags/dag_scraping_html.py
+++ airflow/dags/dag_scraping_html.py
@@ -20,8 +20,8 @@ BCE_BASE_URL = "https://kbopub.economie.fgov.be"
 )
 def dag_scraping_html():
     entreprises_test = [
-        {"entity_number": "0200.065.765", "denomination": "Test Company 1"},
-        {"entity_number": "0200.068.636", "denomination": "Test Company 2"},
+        {"entity_number": "0200.362.408", "denomination": "Test Company 1"},
+        {"entity_number": "0200.420.410", "denomination": "Test Company 2"},
     ]
 
     @task()
@@ -69,7 +69,7 @@ def dag_scraping_html():
         client = InsecureClient(NAMENODE_URL, user="root")
 
         entity_number = entreprise["entity_number"]
-        max_retries = len(proxy_service.proxies)
+        max_retries = 30
 
         print(f"🏢 Scraping entreprise: {entity_number}")
 
@@ -85,6 +85,7 @@ def dag_scraping_html():
                 "entity_number": entity_number,
             }
 
+        print(f"max_retries: {max_retries}")
         for attempt in range(max_retries):
             try:
                 print(f"🔄 Tentative {attempt + 1}/{max_retries}")
@@ -113,13 +114,14 @@ def dag_scraping_html():
 
                 # Faire la requête
                 proxies_dict = proxy_service.get_proxy_dict(proxy)
+                print(f"🌐 URL: {url}")
                 response = requests.get(
                     url,
                     proxies=proxies_dict,
                     timeout=15,
                     headers={"User-Agent": "Mozilla/5.0"},
                 )
-
+                print(f"🌐 Status code: {response.status_code}")
                 # Gérer 404
                 if response.status_code == 404:
                     print(f"⚠️ Entreprise {entity_number} non trouvée (404)")
@@ -166,7 +168,6 @@ def dag_scraping_html():
                     "reason": str(e),
                     "entity_number": entity_number,
                 }
-
         return {
             "status": "failed",
             "reason": "Échec après toutes les tentatives",
